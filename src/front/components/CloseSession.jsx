d4aa66e2d3eb41fda399cfc70baf67c91673f692
Carlos Melchor Perez <carloos0665@gmail.com>
2025-05-14 22:35:54 +0000

    agregar componentes closession, privateroute

---
+++ src/front/components/CloseSession.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
+
+export const CloseSession = () => {
+    const navigate = useNavigate();
+    const { dispatch } = useGlobalReducer();
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+
+        dispatch({ type: "LOGOUT" });
+
+        navigate("/ogin");
+    };
+
+    return (
+        <>
+            <button className="btn btn-danger" onClick={handleLogout}>Cerrar sesión</button>
+        </>
+    );
+};
