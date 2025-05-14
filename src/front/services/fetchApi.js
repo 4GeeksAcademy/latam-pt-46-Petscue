f50b2f2835cfa30c7b50f240f88f809c8173baaa
Carlos Melchor Perez <carloos0665@gmail.com>
2025-05-14 22:37:44 +0000

    cambiar nombre de la carpeta

---
+++ src/front/services/fetchApi.js
@@ -0,0 +1,28 @@
+const API_URL =
+  "https://urban-memory-g4r4rwjv69x4cwr6r-3001.app.github.dev/api";
+
+export const tokenLogin = async (email, password) => {
+  try {
+    const response = await fetch(`${API_URL}/login`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const data = await response.json();
+    if (!response.ok) {
+      throw new Error(data.message || "Unknown error");
+    }
+
+    if (data.token) {
+      localStorage.setItem("token", data.token);
+    }
+
+    return data;
+  } catch (error) {
+    console.error("Error during login:", error);
+    throw error;
+  }
+};
