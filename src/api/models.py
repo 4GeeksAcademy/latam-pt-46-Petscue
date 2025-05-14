a2437f07f2a5c75128cb2f14e50363335e021271
BasCart <b.cartagena.paredes@gmail.com>
2025-05-14 17:56:49 +0000

    add a first model, user

--- src/api/models.py
+++ src/api/models.py
@@ -1,11 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    last_name: Mapped[str] = mapped_column(String(120), nullable=False)
+    phone: Mapped[int] = mapped_column(Integer, nullable= False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
@@ -14,6 +17,10 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "phone": self.phone,
             "email": self.email,
+
             # do not serialize the password, its a security breach
         }
