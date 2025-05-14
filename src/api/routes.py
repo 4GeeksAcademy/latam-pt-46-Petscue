040adb9687b472562de65058dc1184ed20babc48
BasCart <b.cartagena.paredes@gmail.com>
2025-05-14 17:57:38 +0000

    add a route new_user

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,18 +5,34 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from api.dataStructure import AllTheUsers
 
 api = Blueprint('api', __name__)
 
 # Allow CORS requests to this API
 CORS(api)
 
+members = AllTheUsers()
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+@api.route('/newUser', methods=['POST'])
+def a_new_user():
+    request_body: request.json
+    if "first_name" not in request_body or request_body["first_name"] is None or request_body["first_name"] == "" :
+        return jsonify({"message": "Data error"})
+    
+    elif "last_name" not in request_body or request_body["last_name"] is None or request_body["last_name"] == "" :
+        return jsonify({"message": "Data error"})
+        
+    elif "email" not in request_body or request_body["email"] is None or request_body["email"] == "" :
+        return jsonify({"message": "Data error"})
+    
+    elif "phone" not in request_body or request_body["phone"] is None or request_body["phone"] == "" :
+        return jsonify({"message": "Data error"})
+    
+    elif "password" not in request_body or request_body["password"] is None or request_body["password"] == "" :
+        return jsonify({"message": "Data error"})
+    
+    else:
+        new_member = members.append(request_body)
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
-
-    return jsonify(response_body), 200
+        return jsonify(new_member), 201
