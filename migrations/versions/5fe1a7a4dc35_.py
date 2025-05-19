30eb23c9c236e740aaae5b8c1771a0b1b9c5d2c6
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-05-19 04:42:48 +0000

    remove button tag from the links

--- migrations/versions/5fe1a7a4dc35_.py
+++ migrations/versions/5fe1a7a4dc35_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 03526c493c45
+Revision ID: 5fe1a7a4dc35
 Revises: 
-Create Date: 2025-05-19 03:08:41.029945
+Create Date: 2025-05-19 04:24:24.310961
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '03526c493c45'
+revision = '5fe1a7a4dc35'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
-  const { store } = useGlobalReducer()
+	const { store } = useGlobalReducer()
 	console.log("este es el token: ", store.token)
 
 	return (
@@ -14,18 +14,18 @@ export const Navbar = () => {
 				<div className="ml-auto gap-3 d-flex align-items-center">
 					{store.token ?
 						<>
-						<Link to="/profile" className="nav-bar-link m-auto" >
-						<button className="btn btn-warning">Profile</button>
-					</Link>
+							<Link to="/profile" className="nav-bar-link m-auto btn-lemon" >
+								Profile
+							</Link>
 						</> :
-						
-						<>	<Link to="/login" className="nav-bar-link m-auto" >
-						<button className="btn btn-lemon">Access your Account</button>
-					</Link>
-					<Link to="/register" className="">
-						<button className="btn btn-orange">Register</button>
-					</Link>
-					</>}
+
+						<>	<Link to="/login" className="nav-bar-link m-auto btn-lemon text-black" >
+							Access your Account
+						</Link>
+							<Link to="/register" className="btn-orange">
+								Register
+							</Link>
+						</>}
 
 				</div>
 			</div>
