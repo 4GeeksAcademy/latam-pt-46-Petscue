74b8624227f9f2d26f10721a5afc4b57beb18bd5
Alison Estephany <96319139+Alisoneoz@users.noreply.github.com>
2025-05-20 03:38:03 +0000

    correct routes to include the 'api' from th blueprint and prevent the 405 error

--- src/front/services/createUser.js
+++ src/front/services/createUser.js
@@ -3,7 +3,7 @@ const API_URL = import.meta.env.VITE_BACKEND_URL;
 export const newUser = async (user) => {
   try {
     console.log("Enviando datos a /newUser:", user);
-    const response = await fetch(`${API_URL}/newUser`, {
+    const response = await fetch(`${API_URL}/api/newUser`, {
       method: "POST",
       body: JSON.stringify(user),
       headers: {
