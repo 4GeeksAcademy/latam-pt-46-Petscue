e050051b39a8027eabd708628724b5ab473cdd4d
BasCart <b.cartagena.paredes@gmail.com>
2025-05-24 04:17:07 +0000

    add profile animal

--- src/front/components/SideBar.jsx
+++ src/front/components/SideBar.jsx
@@ -3,10 +3,10 @@ import { FaPaw, FaUserAlt } from "react-icons/fa";
 
 export const SideBar = () => {
     return (
-         <div className="  d-flex flex-column py-4 px-3 min-vh-100 blue text-white">
+        <div className="  d-flex flex-column py-4 px-3 min-vh-100 blue text-white">
             <div className="mb-4 text-center">
                 <h4 className="fw-bold mb-0">
-                     Petscue
+                    Petscue
                 </h4>
                 <p className="mt-1">Your Panel</p>
             </div>
@@ -18,7 +18,13 @@ export const SideBar = () => {
                     </Link>
                 </li>
                 <li className="nav-item">
-                    <Link to="/profile/newanimal" className="nav-link d-flex align-items-center text-white">
+                    <Link to="profileanimal" className="nav-link d-flex align-items-center  text-white">
+                        <span className="me-2"><FaPaw /></span>
+                        Perfil del animal
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="newanimal" className="nav-link d-flex align-items-center text-white">
                         <span className="me-2"><FaUserAlt /></span>
                         Add New Animal
                     </Link>
