88037d6c86fc4ca5f4067faec3cc3c99138fbb79
BasCart <b.cartagena.paredes@gmail.com>
2025-05-27 01:40:58 +0000

    add story

--- src/front/pages/privatePageForAll.jsx
+++ src/front/pages/privatePageForAll.jsx
@@ -2,52 +2,61 @@ import { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { privateMenu } from "../services/privateMenu";
 
-export const AllDashboard = () =>{
-    const[roleData, setRoleData] = useState(null);
+export const AllDashboard = () => {
+    const [roleData, setRoleData] = useState(null);
     const navigate = useNavigate()
 
-    const routePrivateForAll = async() => {
-        try{
+    const routePrivateForAll = async () => {
+        try {
             const data = await privateMenu()
 
-            if(data && data.user_data && data.user_data.role === `OWNER`){
+            if (data && data.user_data && data.user_data.role === `OWNER`) {
                 setRoleData(data.user_data)
-            } 
-            else if(data && data.user_data && data.user_data.role === `ADOPTER`){
+            }
+            else if (data && data.user_data && data.user_data.role === `ADOPTER`) {
                 setRoleData(data.user_data)
-            } 
-            else if(data && data.user_data && data.user_data.role === `RESCUER`){
+            }
+            else if (data && data.user_data && data.user_data.role === `RESCUER`) {
                 setRoleData(data.user_data)
             } else {
                 console.warn(`User is not exist: `, data.user_data)
                 navigate("/login")
             }
-        }catch(error){
+        } catch (error) {
             console.error("Ocurrio un error al acceder a la información, ", error)
         }
-        
+
     }
 
-    useEffect(() => { 
+    useEffect(() => {
         routePrivateForAll()
-    },[])
+    }, [])
 
-    return(
+    return (
         <div>
             {roleData ? (
-            <div className="card">
-                <div className="card-body">
-                    <div className="card-text">
-                        <strong>Name :</strong> {roleData.first_name} {roleData.last_name}
+                <>
+                    <div className="card">
+                        <div className="card-body">
+                            <h3 className="card-title">
+                                Profile:
+                            </h3>
+                            <div className="card-text">
+                                <strong>Name :</strong> {roleData.first_name} {roleData.last_name}
+                            </div>
+                            <div className="card-text">
+                                <strong>Phonen :</strong> {roleData.phone}
+                            </div>
+                            <div className="card-text">
+                                <strong>Email :</strong> {roleData.email}
+                            </div>
+                        </div>
+                        <div className="card mt-3">
+                            <h3 className="card-title">Story :</h3>
+                            <p className="card-text">{roleData.story}</p>
+                        </div>
                     </div>
-                    <div className="card-text">
-                        <strong>Phonen :</strong> {roleData.phone}
-                    </div>
-                    <div className="card-text">
-                        <strong>Email :</strong> {roleData.email}
-                    </div>
-                </div>
-            </div>
+                </>
             ) : (
                 <p>Cargando información del propietario...</p>
             )}
