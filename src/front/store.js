74c3e30538430026a0cee873c43309bab8d582fb
Carlos Melchor Perez <carloos0665@gmail.com>
2025-05-28 17:00:18 +0000

    corregir store

--- src/front/store.js
+++ src/front/store.js
@@ -26,26 +26,24 @@ export default function storeReducer(store, action = {}) {
     case "LOGOUT":
       return { ...store, token: "", user: null };
 
-    case "TOGGLE_FAVORITE":
-      const id = action.payload;
-      const isFavorite = store.favorites.includes(id);
+    case "SET_FAVORITES":
       return {
         ...store,
-        favorites: isFavorite
-          ? store.favorites.filter((fav) => fav !== id)
-          : [...store.favorites, id],
+        favorites: action.payload,
+      };
+
+    case "TOGGLE_FAVORITE":
+      return {
+        ...store,
+        favorites: store.favorites.includes(action.payload)
+          ? store.favorites.filter((id) => id !== action.payload)
+          : [...store.favorites, action.payload],
       };
 
     case "SET_FILTERS":
       return {
         ...store,
         filters: action.payload,
-      };
-
-    case "SET_SHOW_FAVORITES":
-      return {
-        ...store,
-        showFavorites: action.payload,
       };
 
     case "SET_ROLE":
