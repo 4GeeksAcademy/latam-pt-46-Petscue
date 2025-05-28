9bc17c609e2a5938c374d1b30f766a3f6ed7dabb
Carlos Melchor Perez <carloos0665@gmail.com>
2025-05-28 16:58:43 +0000

    arreglar estilos

--- src/front/components/PetCardFav.jsx
+++ src/front/components/PetCardFav.jsx
@@ -1,17 +1,30 @@
 import React from "react";
 import { Card, Button, Col } from "react-bootstrap";
 
-export const PetCardFav = ({ photo, name, race, age, toggleFavorite, isFavorite, id, description }) => {
+export const PetCardFav = ({
+  photo,
+  name,
+  race,
+  age,
+  toggleFavorite,
+  isFavorite,
+  id,
+  description,
+}) => {
   return (
     <Col xs={12} sm={6} md={4} lg={3}>
       <Card className="pet-card">
-        <Card.Img variant="top" src={typeof photo === "string" ? photo : photo?.image} />
+        <Card.Img
+          variant="top"
+          src={typeof photo === "string" ? photo : photo?.image}
+        />
         <Card.Body className="text-center">
-          <Card.Title>{name}</Card.Title>
-          <Card.Text>{race}</Card.Text>
-          <Card.Text className="text-muted">{age}</Card.Text>
-          <Card.Text>{description}</Card.Text>
+          <Card.Title className="card-Title">Name: {name}</Card.Title>
+          <Card.Text className="Card-Text">Breed: {race}</Card.Text>
+          <Card.Text className="text-muted Card-Text">Age: {age}</Card.Text>
+          <Card.Text className="Card-Text">{description}</Card.Text>
           <Button
+            className="Card-Text"
             variant={isFavorite ? "danger" : "outline-primary"}
             onClick={() => toggleFavorite(id)}
           >
