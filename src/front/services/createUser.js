784e05826084f79278b3bafa5777bb9132a95eb7
Carlos Melchor Perez <carloos0665@gmail.com>
2025-05-30 00:51:09 +0000

    quitar console.log

--- src/front/services/createUser.js
+++ src/front/services/createUser.js
@@ -2,7 +2,6 @@ const API_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const newUser = async (user) => {
   try {
-    console.log("Enviando datos a /newUser:", user);
     const response = await fetch(`${API_URL}/api/user`, {
       method: "POST",
       body: JSON.stringify(user),
