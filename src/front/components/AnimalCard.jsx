cba29deffeeb6262f54d707e4fc2a080adceb91e
Carlos Melchor Perez <carloos0665@gmail.com>
2025-06-02 06:36:36 +0000

    agregar funciones con botones puy y delete

--- src/front/components/AnimalCard.jsx
+++ src/front/components/AnimalCard.jsx
@@ -1,12 +1,23 @@
-export const AnimalCard = ({ name, race, age, description, photo }) => {
-  return (    
+import { Button } from "react-bootstrap";
+
+export const AnimalCard = ({
+  id,
+  name,
+  race,
+  age,
+  description,
+  photo,
+  onEdit,
+  handleDelete,
+}) => {
+  return (
     <div className="card shadow-sm col-3 card  px-0 pt-0 pb-3 border rounded-5 ">
       <img
         src={photo}
         className=" border rounded-5 img-animal-card "
         alt="Animal"
       />
-    
+
       <div className="card-body d-flex flex-column ">
         <h5 className="card-title d-flex justify-content-center fs-2">
           Name: {name}
@@ -17,9 +28,17 @@ export const AnimalCard = ({ name, race, age, description, photo }) => {
         <p className="card-text d-flex justify-content-center fs-5">
           Age: {age}
         </p>
-        <p className="description card-text  d-flex justify-content-center  fs-5 text-center">
+        <p className="description card-text  d-flex justify-content-center fs-5 text-center overflow-hidden overflow-y-scroll custom-scroll-2">
           {description}
         </p>
+        <div className="d-flex justify-content-center gap-4 flew-nowrap">
+          <Button variant="outline-primary" onClick={() => onEdit(id)}>
+            Edit
+          </Button>
+          <Button variant="outline-danger" onClick={() => handleDelete(id)}>
+            Delete animal
+          </Button>
+        </div>
       </div>
     </div>
   );
