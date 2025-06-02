f484ae2f8405bbc8d8cef4bd367633b6af28b3db
BasCart <b.cartagena.paredes@gmail.com>
2025-06-02 04:51:54 +0000

    update del filtro

--- src/front/components/AnimalFilters.jsx
+++ src/front/components/AnimalFilters.jsx
@@ -1,15 +1,29 @@
-export const AnimalFilters = () => {
+import { light } from "@cloudinary/url-gen/qualifiers/fontWeight"
+import React from "react"
+
+export const AnimalFilters = ({selectedAnimalType, onSelectedAnimalType}) => {
   return (
     <div className="d-flex justify-content-center align-items-center mb-4 gap-2">
     
-    <button className="btn btn-light border rounded-pill px-3 py-2 d-flex align-items-center gap-2 " type="button">
+    <button 
+      className={`btn btn-light border rounded-pill px-3 py-2 d-flex align-items-center gap-2 ${selectedAnimalType === "cat" ? "btn-primary text-white" : "btn-light"}`} 
+      type="button"
+      onClick={() => onSelectedAnimalType("cat")}
+    >
       <span>🐱</span> Cats
     </button>
-    <button className="btn btn-light border rounded-pill px-3 py-2 d-flex align-items-center gap-2 " type="button">
+    <button
+      className={`btn btn-light border rounded-pill px-3 py-2 d-flex align-items-center gap-2 ${selectedAnimalType === "dog" ? "btn-primary text-white" : "btn-light"}`} 
+      type="button"
+      onClick={() => onSelectedAnimalType("dog")}
+    >
       <span >🐶</span> Dogs
     </button>
-    <button className="btn btn-light border rounded-pill px-3 py-2 d-flex align-items-center gap-2 " type="button">
-      <span >🐰</span> Rabits
+    <button
+      className={`btn btn-light border rounded-pill px-3 py-2 d-flex align-items-center gap-2 ${selectedAnimalType === "rabbit" ? "btn-primary text-white" : "btn-light"}`} 
+      type="button"
+      onClick={() => onSelectedAnimalType("rabbit")}
+    >      <span >🐰</span> Rabbits
     </button>
   </div>
   )
