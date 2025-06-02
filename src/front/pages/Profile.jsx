68aca47873accacf89a620c2fe19f7de8889f052
Carlos Melchor Perez <carloos0665@gmail.com>
2025-06-02 06:39:01 +0000

    botones delete y edit y alerta para cancelar o eliminar

--- src/front/pages/Profile.jsx
+++ src/front/pages/Profile.jsx
@@ -2,9 +2,14 @@ import { AnimalFilters } from "../components/AnimalFilters";
 import { AnimalCard } from "../components/AnimalCard";
 import { getMyAnimals } from "../services/getMyAnimals";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { deleteAnimal } from "../services/deleteAnimal";
+import toast, { Toaster } from "react-hot-toast";
+import { Button } from "react-bootstrap";
 
 export const Profile = () => {
   const [myAnimals, setMyAnimals] = useState([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchAnimals = async () => {
@@ -18,22 +23,60 @@ export const Profile = () => {
     fetchAnimals();
   }, []);
 
+  const handleEdit = (id) => {
+    navigate(`/profile/edit/${id}`);
+  };
+
+  const handleDelete = (id) => {
+    toast((t) => (
+      <span>
+        Are you sure you want to <b>delete</b> this animal?
+        <div className="d-flex justify-content-center">
+          <Button
+            className="mx-4"
+            variant="outline-primary"
+            onClick={async () => {
+              try {
+                await deleteAnimal(id);
+                toast.success("Animal deleted");
+                toast.dismiss(t.id);
+                navigate("/profile");
+              } catch (error) {
+                toast.error("Error deleting animal");
+                toast.dismiss(t.id);
+              }
+            }}
+          >
+            Yes
+          </Button>
+          <Button variant="outline-danger" onClick={() => toast.dismiss(t.id)}>
+            Cancel
+          </Button>
+        </div>
+      </span>
+    ));
+  };
+
   return (
     <div className="">
+      <Toaster position="top-center" reverseOrder={false} />
       <div className=" d-flex justify-content-around my-5">
         <h2 className="mb-4 text-center">Uploaded Animals</h2>
         <AnimalFilters />
       </div>
 
-      <div className="row gap-3 d-flex justify-content-center pb-5">
+      <div className="row gap-4 d-flex justify-content-center pb-5">
         {myAnimals.map((animal) => (
           <AnimalCard
             key={animal.id}
+            id={animal.id}
             age={animal.age}
             name={animal.name}
             race={animal.race}
             description={animal.description}
             photo={animal.photo}
+            onEdit={handleEdit}
+            handleDelete={handleDelete}
           />
         ))}
       </div>
